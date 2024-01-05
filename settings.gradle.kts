04a02999cceee9b59f31433700611bbed4d70392
grumblefoot <70971715+grumblefoot@users.noreply.github.com>
2024-01-05 05:35:37 -0800

    Inital commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CodepathAND102Prework_HelloWorld"
+include(":app")
